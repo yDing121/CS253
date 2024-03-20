41cdde23a2496091badb26950c81cb6910a4a16c
Yilang (Lance) Ding <ydingus1@gmail.com>
2024-03-20 18:30:38 -0400

    Question 2 complete

--- Project2/Runner.java
+++ Project2/Runner.java
@@ -4,31 +4,66 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.io.File;
-import java.util.Stack; 
 
 public class Runner {
     final static String FPATH = "cleandata.txt";
 
     public static void main(String[] args) throws FileNotFoundException {
         HashMap<String, ArrayList<String>> adjlist = read(FPATH);
+        HashMap<String, ArrayList<String>> revlist = reverseAdj(adjlist);
+        // int i = 0;
+        // for (String k : adjlist.keySet()){
+        //     System.out.println(++i + "\t" + k + "\t" + adjlist.get(k));
+        // }
+
+        System.out.println("-----Reverse Adjacency List-----");
         int i = 0;
-        for (String k : adjlist.keySet()){
-            System.out.println(++i + "\t" + k + adjlist.get(k));
+
+        for (String k : revlist.keySet()){
+            System.out.println(++i + "\t" + k + "\t" + revlist.get(k));
         }
 
-        // topSort(adjlist);
-        // HashSet<String> visited = new HashSet<String>();
-        // ArrayList<String> post = new ArrayList<String>();
-        // dfs("MATH 1591", visited, adjlist, post);
+        System.out.println("----------");
+
         ArrayList<String> post = topSort(adjlist);
         
         for (i=post.size()-1; i>=0; i--){
             System.out.println(post.get(i));
         }
+
+        System.out.println("-----Get Prereqs-----");
+        System.out.println(getPrereqs("CSCI 4490", revlist));
+        // System.out.println();
+        prereqRunner(revlist);
+    }
+
+    public static ArrayList<String> getPrereqs(String course, HashMap<String, ArrayList<String>> revlist){
+        HashSet<String> visited = new HashSet<String>();
+        ArrayList<String> post = new ArrayList<String>();
+
+        dfs(course, visited, revlist, post);
+        return post;
+    }
+
+    public static void prereqRunner(HashMap<String, ArrayList<String>> revlist){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input a course name and number e.g. CSCI 2440");
+        String query = scanner.nextLine().strip();
+
+        for (String s: getPrereqs(query, revlist)){
+            System.out.println(s);
+        }
+        scanner.close();
     }
 
     public static void dfs(String key, HashSet<String> visited, HashMap<String, ArrayList<String>> alist, ArrayList<String> post){
         visited.add(key);
+
+        if (alist.get(key) == null){
+            post.add(key);
+            return;
+        }
+
         for (String s: alist.get(key)){
             if (!visited.contains(s)){
                 dfs(s, visited, alist, post);
@@ -41,11 +76,9 @@ public class Runner {
     public static ArrayList<String> topSort(HashMap<String, ArrayList<String>> alist)
     {
         HashSet<String> visited = new HashSet<String>();
-        // Stack<String> stack = new Stack<String>();
         ArrayList<String> post = new ArrayList<String>();
 
         while (visited.size() != alist.keySet().size()){
-            // Find first unvisited element
             String target = null;
             for (String s: alist.keySet()){
                 if (!visited.contains(s)){
@@ -53,25 +86,24 @@ public class Runner {
                     break;
                 }
             }
-
             dfs(target, visited, alist, post);
-
-
-            // if (stack.empty()){
-            //     // Find first unvisited element
-            //     for (String s: alist.keySet()){
-            //         if (!visited.contains(s)){
-            //             stack.add(s);
-            //             visited.add(s);
-            //             break;
-            //         }
-            //     }
-
-            //     System.out.println(stack);
-            // }
-            // // stack.pop();
         }
         return post;
+    }
+
+    public static HashMap<String, ArrayList<String>> reverseAdj(HashMap<String, ArrayList<String>> alist){
+        HashMap<String, ArrayList<String>> revlist = new HashMap<String, ArrayList<String>>();
+
+        for (String s: alist.keySet()){
+            for (String key: alist.get(s)){
+                if (!revlist.containsKey(key)){
+                    revlist.put(key, new ArrayList<String>());
+                }
+                revlist.get(key).add(s);
+            }
+        }
+
+        return revlist;
     }
 
     public static HashMap<String, ArrayList<String>> read(String fpath) throws FileNotFoundException {
@@ -91,7 +123,7 @@ public class Runner {
             if (pos >= 0) {
                 String prereqs = line.substring(pos + 1, line.length() - 1);
                 String[] plist = prereqs.split(",");
-                System.out.println(course + "\n" + prereqs + "\n-------------\n");
+                // System.out.println(course + "\n" + prereqs + "\n-------------\n");
 
                 for (String prereq : plist){
                     prereq = prereq.strip();
